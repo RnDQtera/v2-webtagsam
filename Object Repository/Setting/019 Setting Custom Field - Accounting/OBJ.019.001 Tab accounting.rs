ed682f4cb6a8b5361f3f45ececc8d574d584549d
quality-assurance01 <qa-1@qwertysytem.net>
2024-06-12 14:44:37 +0700

    011 Setting custom field accounting

---
+++ Object Repository/Setting/019 Setting Custom Field - Accounting/OBJ.019.001 Tab accounting.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OBJ.019.001 Tab accounting</name>
+   <tag></tag>
+   <elementGuidId>61cd8efc-21d7-4b9e-a316-33f8a4991acb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/main/div/div/div[2]/div/div/div[1]/ul/li[3]/a/span</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Setting/019 Setting Custom Field - Accounting/OBJ.019.003 Option currency.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OBJ.019.003 Option currency</name>
+   <tag></tag>
+   <elementGuidId>2c377b4d-6534-4b59-8b13-f8d992921e87</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[7]/div/ul/li[6]/span</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
