645c6da02ce08d177c8362482ffa06b6ae154b14
RnDQtera <bagus@qtera.co.id>
2024-07-22 11:05:41 +0700

    Restruction Folder

---
+++ Object Repository/04 Addon/001 Audit/OBJ.001.001.rs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OBJ.001.001</name>
+   <tag></tag>
+   <elementGuidId>b27e0004-6a98-412e-997f-29c44b04fea7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Test Suites/TS-Group-003.ts
+++ Test Suites/TS-Group-003.ts
@@ -15,42 +15,42 @@
       <guid>b6ff53d4-625e-4171-8962-f945ad77a463</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 TC.Setting/003 Setting Group/TC.SET.003.001 Create Group</testCaseId>
+      <testCaseId>Test Cases/Transaction/6 Transfer/1 TC.Setting/003 Setting Group/TC.SET.003.001 Create Group</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>1297beb3-a077-4ffb-b978-02a6704b52e9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 TC.Setting/003 Setting Group/TC.SET.003.002 Edit Group</testCaseId>
+      <testCaseId>Test Cases/Transaction/6 Transfer/1 TC.Setting/003 Setting Group/TC.SET.003.002 Edit Group</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>db3df2f4-11e4-4b21-a09a-8a4c5a3630b5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 TC.Setting/003 Setting Group/TC.SET.003.003 Create Subgroup</testCaseId>
+      <testCaseId>Test Cases/Transaction/6 Transfer/1 TC.Setting/003 Setting Group/TC.SET.003.003 Create Subgroup</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>058229bd-d527-4e25-b4cc-c260b0250c31</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 TC.Setting/003 Setting Group/TC.SET.003.004 Search Data Table Group</testCaseId>
+      <testCaseId>Test Cases/Transaction/6 Transfer/1 TC.Setting/003 Setting Group/TC.SET.003.004 Search Data Table Group</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>deefce67-c900-47e9-af07-f91a16a2dc26</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 TC.Setting/003 Setting Group/TC.SET.003.005 Enable and Disable Disposal Group</testCaseId>
+      <testCaseId>Test Cases/Transaction/6 Transfer/1 TC.Setting/003 Setting Group/TC.SET.003.005 Enable and Disable Disposal Group</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>0aed7183-ca2a-4386-8a6e-2bb6478c9791</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 TC.Setting/003 Setting Group/TC.SET.003.005 Disable and Enable Disposal Group</testCaseId>
+      <testCaseId>Test Cases/Transaction/6 Transfer/1 TC.Setting/003 Setting Group/TC.SET.003.005 Disable and Enable Disposal Group</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
